6f6ae0706111d9c72d52aeea8b7e1336395f53ab
Michelle <>
2014-09-02 20:05:26 -0700

    UserTab.java Added: buttonAddUser and xml, popupView for inflating popupEditText (PopupWindow), buttonEditText, editTextinPopUp
    *Logic notes: need logic for inserting user via editTextInPopUp with buttonEditText
    	*need logic for closing popup at any time
    	*need logic for checking if user exists, and disallowing insertion
    *Dev notes: consider making custom view for creategarment tab, ultimately? could have created a custom view for the popup but not sure yet if create garment tab logic bulk should be in a popup. not even sure if i like popups

--- src/ootd/com/UserTab.java
+++ src/ootd/com/UserTab.java
@@ -6,6 +6,7 @@
 
 package ootd.com;
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.*;
 import android.widget.*;
@@ -29,7 +30,7 @@ public class UserTab extends Fragment {
         Log.d("UserDAO","opening usource connection");
         UserDAO u_source = new UserDAO(this.getActivity());
         u_source.open();
-        u_source.insertUser("michelle");
+        //u_source.insertUser("michelle");
         ///List container for user objects
         ArrayList<User> users = u_source.selectAllUsers();
         ///instantiate View representation for list of user objects, defined in user_tab.xml
@@ -40,11 +41,31 @@ public class UserTab extends Fragment {
         
         //button to open popupedittext
         Button buttonAddUser = (Button)inflaterView.findViewById(R.id.ButtonAddUser);
-        buttonAddUser.setOnClickListener(new View.OnClickListener() {
-             public void onClick(View v) {
-                 PopUpEditText popup = new PopUpEditText(container,inflater);
-             }
-         });
+        Log.d("UserTab.java","buttonAddUser =" + buttonAddUser);
+        buttonAddUser.setOnClickListener(new View.OnClickListener() 
+        {
+            public void onClick(View v) 
+            {
+                Log.d("UserTab.java", "buttonAddUser clicked");
+                View popupView = getActivity().getLayoutInflater().inflate(R.layout.user_edittext_popup, (ViewGroup)getActivity().findViewById(R.id.popupEditText));
+                Log.d("PopUpEditText.java", "popupView =" + popupView);
+                PopupWindow popupEditText = new PopupWindow(popupView, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, true);
+                popupEditText.showAtLocation(getActivity().findViewById(R.id.ButtonAddUser), Gravity.CENTER, 0,0);
+                popupEditText.update(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                Button buttonEditText = (Button) popupView.findViewById(R.id.ButtonEditText);
+                EditText editTextInPopUp = (EditText) popupView.findViewById(R.id.editTextInPopUp);
+                Log.d("UserTab.java", "buttonEditText =" + buttonEditText);
+                buttonEditText.setOnClickListener(new View.OnClickListener() 
+                {
+                    public void onClick(View v) 
+                    {
+                 //
+                        //
+                    }
+                });
+                
+            }
+        });
         return inflaterView;
     }
 }
